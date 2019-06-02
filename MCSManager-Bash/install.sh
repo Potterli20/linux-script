e1332b41fe6792ef76e295bff0e3e662ec4e6a91
potterli <potterli520@gmail.com>
2019-06-02 17:54:55 +0800

    update install.sh 1.0

--- MCSManager-Bash/install.sh
+++ MCSManager-Bash/install.sh
@@ -1,6 +1,63 @@
 #!/bin/bash
 export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+export PATH
 
+#=================================================
+#	System Required: CentOS 6/7,Debian 8/9,Ubuntu 16+
+#	Description: MC服务器管理
+#	Version: 1.0
+#=================================================
+
+sh_ver="1.0"
+github="raw.githubusercontent.com/Potterli20/linux-script/master/MCSManager-Bash"
+
+#更新脚本
+Update_Shell(){
+	echo -e "当前版本为 [ ${sh_ver} ]，开始检测最新版本..."
+	sh_new_ver=$(wget --no-check-certificate -qO- "http://${github}/install.sh"|grep 'sh_ver="'|awk -F "=" '{print $NF}'|sed 's/\"//g'|head -1)
+	[[ -z ${sh_new_ver} ]] && echo -e "${Error} 检测最新版本失败 !" && start_menu
+	if [[ ${sh_new_ver} != ${sh_ver} ]]; then
+		echo -e "发现新版本[ ${sh_new_ver} ]，是否更新？[Y/n]"
+		read -p "(默认: y):" yn
+		[[ -z "${yn}" ]] && yn="y"
+		if [[ ${yn} == [Yy] ]]; then
+			wget -N --no-check-certificate http://${github}/install.sh && chmod +x install .sh
+			echo -e "脚本已更新为最新版本[ ${sh_new_ver} ] !"
+		else
+			echo && echo "	已取消..." && echo
+		fi
+	else
+		echo -e "当前已是最新版本[ ${sh_new_ver} ] !"
+		sleep 5s
+	fi
+}
+
+#开始菜单
+start_menu(){
+clear
+echo && echo -e " MC服务器管理 ${Red_font_prefix}[v${sh_ver}]${Font_color_suffix}
+  -- 就是爱生活 | MC服务器管理 --
+  
+ ${Green_font_prefix}0.${Font_color_suffix} 升级脚本
+ ${Green_font_prefix}1.${Font_color_suffix} 退出脚本
+————————————————————————————————" && echo
+
+echo
+read -p " 请输入数字 [0-1]:" num
+case "$num" in
+	0)
+	Update_Shell
+   	1)
+	exit 1
+	;;
+	*)
+	clear
+	echo -e "${Error}:请输入正确数字 [0-1]"
+	sleep 5s
+	start_menu
+	;;
+esac
+}
 
 #Check Root
 [ $(id -u) != "0" ] && { echo "Error: You must be root to run this script"; exit 1; }
@@ -37,11 +94,10 @@ workdir=$(pwd)
 
 #Install Basic Tools
 if [[ ${OS} == Ubuntu ]];then
-        echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" >> /etc/apt/sources.list
-	echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" >> /etc/apt/sources.list
-	apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
+        	sudo add-apt-repository ppa:webupd8team/java
 	apt-get update
-	apt-get install oracle-java8-installer -y
+	apt-get install oracle-java8-installer default-jdk -y
+	apt install software-properties-common dirmngr -y
 	apt-get install git git-core curl build-essential openssl libssl-dev wget -y
 	git clone https://github.com/nodejs/node.git
     	chmod -R 755 node
@@ -59,11 +115,10 @@ if [[ ${OS} == CentOS ]];then
 	make && make install
 fi
 if [[ ${OS} == Debian ]];then
-        echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" >> /etc/apt/sources.list
-	echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" >> /etc/apt/sources.list
-	apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
+	add-apt-repository ppa:webupd8team/java
 	apt-get update
-	apt-get install oracle-java8-installer -y
+	apt-get install oracle-java8-installer default-jdk -y
+	apt install software-properties-common dirmngr -y
 	apt-get install git git-core curl build-essential openssl libssl-dev wget -y
 	git clone https://github.com/nodejs/node.git
     	chmod -R 755 node
